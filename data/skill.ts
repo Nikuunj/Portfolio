0c52207ba5e97bcbe51d12dbd7b7f4facb7c2aad
Nikunj <mnikunj262@gmail.com>
2025-07-23 21:36:16 +0530

    fix speling

--- data/skill.ts
+++ data/skill.ts
@@ -1,4 +1,4 @@
-export const skills = ['TypeScript', 'JavaScrip',  'Next.js', 'React.js', 'Recoil',
+export const skills = ['TypeScript', 'JavaScript',  'Next.js', 'React.js', 'Recoil',
      'Node.js', 'Express.js', 'Zod', 'Postman', 'REST', 'WebSocket(ws)', 'HTML5', 'CSS3',
      'Tailwind CSS', 'Prisma', 'MySQL', 'PostgreSQL', 'MongoDB',  'Solana', 'Wallet Adapter', 
      'Git', 'Vite' , 'CI/CD', 'Vercel']
