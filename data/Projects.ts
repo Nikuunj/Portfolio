12fc1319d98f7c647327b820c1a3f1396ab4cc4b
Nikunj <mnikunj262@gmail.com>
2025-10-31 15:06:44 +0530

    update: skill and project

--- data/Projects.ts
+++ data/Projects.ts
@@ -1,60 +1,66 @@
 
 export const projects = [
-     {
-          name: 'Buy me tea - ETH',
-          github: 'https://github.com/Nikuunj/buymetea-eth',
-          live: 'https://buymetea-eth.vercel.app/',
-          describe: 'Platform where anyone can create a personal page and receive ETH tips instantly, without KYC',
-     },
-     {
-          name: 'XStake',
-          github: 'https://github.com/Nikuunj/Staking/',
-          live: 'https://xstake-six.vercel.app/',
-          describe: 'A decentralized staking platform where you can securely stake your ETH and earn rewards, featuring a simple and intuitive interface for seamless interaction.',
+  {
+    name: 'Wing',
+    github: 'https://github.com/Nikuunj/Wing',
+    live: 'https://wing-gamma-nine.vercel.app/',
+    describe: 'Decentralized Tipping Platform on Solana, without KYC',
+  },
+  {
+    name: 'Buy me tea - ETH',
+    github: 'https://github.com/Nikuunj/buymetea-eth',
+    live: 'https://buymetea-eth.vercel.app/',
+    describe: 'Platform where anyone can create a personal page and receive ETH tips instantly, without KYC',
+  },
+  {
+    name: 'XStake',
+    github: 'https://github.com/Nikuunj/Staking/',
+    live: 'https://xstake-six.vercel.app/',
+    describe: 'A decentralized staking platform where you can securely stake your ETH and earn rewards, featuring a simple and intuitive interface for seamless interaction.',
 
-     },
-     {
-          name: 'XA',
-          github: 'https://github.com/Nikuunj/wallet-adapter',
-          live: 'https://wallet-adapter-wine.vercel.app/',
-          describe: 'A Solana wallet adapter. Create Token, Mint Tokens, Send SOL, Sign Messages and Airdrop Wallet' ,
-     },
-     {
-          name: 'XDraw',
-          github: 'https://github.com/Nikuunj/x-draw',
-          live: 'https://x-draw.vercel.app/',
-          describe: 'A Solana wallet adapter. Create Token, Mint Tokens, Send SOL, Sign Messages and Airdrop Wallet' ,
-     },
-     {
-          name: 'MediInfo',
-          github: 'https://github.com/Nikuunj/mediInfo',
-          describe: 'A modern web app that makes accessing and managing healthcare information simple, fast, and user-friendly.' ,
-     },
-     {
-          name: 'X Store',
-          github: 'https://github.com/Nikuunj/x-store',
-          live: 'https://x-store-nine.vercel.app/',
-          describe: 'X-Store is a e-commerce web application that provides a seamless shopping and selling experience.' ,
-          
-     },
-     {
-          name: 'cli postman',
-          github: 'https://github.com/Nikuunj/cli_postman',
-          live: '',
-          describe: 'CLI Postman is a command-line tool that allows users to send HTTP requests directly from the terminal. It supports GET, POST, PUT, PATCH, and DELETE methods.' ,
-          
-     },
-     {
-          name: 'run it',
-          github: 'https://github.com/Nikuunj/run-it.git',
-          live: 'https://run-it.vercel.app/',
-          describe: 'An online compiler to write, edit, and instantly preview HTML, CSS, and JavaScript code in one place.' ,
-     },
+  },
+  {
+    name: 'XA',
+    github: 'https://github.com/Nikuunj/wallet-adapter',
+    live: 'https://wallet-adapter-wine.vercel.app/',
+    describe: 'A Solana wallet adapter. Create Token, Mint Tokens, Send SOL, Sign Messages and Airdrop Wallet',
+  },
+  {
+    name: 'XDraw',
+    github: 'https://github.com/Nikuunj/x-draw',
+    live: 'https://x-draw.vercel.app/',
+    describe: 'A Solana wallet adapter. Create Token, Mint Tokens, Send SOL, Sign Messages and Airdrop Wallet',
+  },
+  {
+    name: 'MediInfo',
+    github: 'https://github.com/Nikuunj/mediInfo',
+    describe: 'A modern web app that makes accessing and managing healthcare information simple, fast, and user-friendly.',
+  },
+  {
+    name: 'X Store',
+    github: 'https://github.com/Nikuunj/x-store',
+    live: 'https://x-store-nine.vercel.app/',
+    describe: 'X-Store is a e-commerce web application that provides a seamless shopping and selling experience.',
+
+  },
+  {
+    name: 'cli postman',
+    github: 'https://github.com/Nikuunj/cli_postman',
+    live: '',
+    describe: 'CLI Postman is a command-line tool that allows users to send HTTP requests directly from the terminal. It supports GET, POST, PUT, PATCH, and DELETE methods.',
+
+  },
+  {
+    name: 'run it',
+    github: 'https://github.com/Nikuunj/run-it.git',
+    live: 'https://run-it.vercel.app/',
+    describe: 'An online compiler to write, edit, and instantly preview HTML, CSS, and JavaScript code in one place.',
+  },
 ]
 
-     // {
-     //      name: '',
-     //      github: '',
-     //      live: '',
-     //      describe: '' ,     
-     // },
+// {
+//      name: '',
+//      github: '',
+//      live: '',
+//      describe: '' ,
+// },
--- data/skill.ts
+++ data/skill.ts
@@ -1,4 +1,4 @@
-export const skills = ['TypeScript', 'JavaScript', 'Rust', 'Solidity',  'Next.js', 'React.js', 'Recoil',
-     'Node.js', 'Express.js', 'Zod', 'Postman', 'REST', 'WebSocket(ws)', 'HTML5', 'CSS3',
-     'Tailwind CSS', 'Prisma', 'MySQL', 'PostgreSQL', 'MongoDB',  'Solana', 'Wallet Adapter', 
-     'Git', 'Vite' , 'CI/CD', 'Docker', 'Vercel'] 
+export const skills = ['TypeScript', 'JavaScript', 'Rust', 'Anchor', 'Solidity', 'Next.js', 'tRPC', 'React.js', 'Recoil',
+  'Node.js', 'Express.js', 'Zod', 'Postman', 'REST', 'WebSocket(ws)', 'HTML5', 'CSS3',
+  'Tailwind CSS', 'Prisma', 'MySQL', 'PostgreSQL', 'MongoDB', 'Solana', 'Wallet Adapter',
+  'Git', 'Vite', 'CI/CD', 'Docker', 'Vercel'] 
